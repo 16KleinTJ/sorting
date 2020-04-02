1d87c32af69601d0c248f5dd8b07613f4e8a47ba
16KleinTJ <62953599+16KleinTJ@users.noreply.github.com>
2020-04-02 16:47:49 +0100

    Implement the bubbleSort method

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,49 +1,52 @@
-﻿using System;
+using System;
 
 namespace SamplePrograms
 {
     class Program
     {
-        /* We'll use instance variables as 'global' variables to avoid having to
-         * mess around with passing arrays between functions
-         */
-
-        private static int[] unsortedList, sortedList;
+        private static int[] Array;
         private static Random random = new Random();
 
         static void Main(string[] args)
         {
-            int i;
-            
-            unsortedList = new int[10];
-            sortedList = new int[10];
+            Array = new int[10];
 
-            for (i = 0; i < unsortedList.Length; i++)
-                unsortedList[i] = random.Next(50);
+            for (int i = 0; i < Array.Length; i++) // fills the array with (10) random integers between 0-50
+                Array[i] = random.Next(50);
 
-            /* You can also do Python-style for loops */
-            foreach (int num in unsortedList)
-                Console.WriteLine(num);
+            Console.Write("Unsorted list:");
+            foreach (int num in Array) // prints each integer in the unsorted array
+                Console.Write(" {0}", num);
 
-            bubbleSort();
+            bubbleSort(); // call the bubblesort method
+
+            Console.Write("\nSorted list:");
+            foreach (int num in Array) // prints each integer in the newly sorted array
+                Console.Write(" {0}", num);
 
             Console.ReadKey();
         }
 
-        /* A void function is a procedure- it does not return a value */
         private static void bubbleSort()
         {
-            int i, j;
-
-            /* Copy unsortedList into sortedList
-             * no, you can't just do sortedList=unsortedList!
-             * https://www.telerik.com/blogs/copying-and-cloning-arrays-in-c
-             */
-
-            /* Iterate over the list, you'll need a couple of counting variables.
-             * Above, i and j are declared for you.
-             * You should know how to do a bubble sort by now, right? :)
-             */
+            while (true) // loops forever until break is called
+            {
+                bool swapped = false; // init a boolean to track whether any swaps have been made
+                for (int i = 0; i < Array.Length - 1; i++) // for the entirety of the list, minus one (so that the final element is not compared against nothing)
+                {
+                    if (Array[i] > Array[i + 1]) // compare the current element to the element to it's right
+                    {
+                        swapped = true; // sets the swapped bool to true indicating that a swap has been made
+                        var tempvar = Array[i]; // store the current element in the array
+                        Array[i] = Array[i + 1]; // overwrites the current element with the smaller value
+                        Array[i + 1] = tempvar; // move the larger value to i + 1
+                    }
+                }
+                if (!swapped) // if swapped is false (indicating that no swaps have been made) the list is sorted
+                {
+                    break; // break is called to end the loop
+                }
+            }
         }
 
     }
